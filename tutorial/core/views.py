7b2ed79ac5c9f5324d27053342a47aa2120fa7b5
Wat <veerawat74@gmail.com>
2021-06-06 23:06:00 +0700

    add variable to html

--- tutorial/core/views.py
+++ tutorial/core/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.http import HttpResponse
+
+
 
 
 # Create your views here.
 def index(request):
-    html = "<h1>Sak</h1>"
+    html = render_to_string('profile.html',
+    { 
+        'name': 'Veerawat Prodpran',
+        'nick_name': 'Wat',
+        'birth_date': '11/08/1993',
+        'email': 'veerawat@odds.team',
+        'tel': '0000000000',
+    }
+    )
     return HttpResponse(html)
